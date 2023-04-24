95601c359c5d545726e915acb5c187a8f0ce89f4
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-04-24 17:52:57 +0200

    Ejemplos varios

---
+++ ejemplos/3.1_Variables/src/variables/EjemploVariables.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class EjemploVariables {
+
+	public static void main(String[] args) {
+		
+				
+		// Declaración, inicialización y asignación
+		
+		String msg = "Hola Mundo";  // Declaración e inicialización
+		int x; // Declaración
+		x = 7;  // Inicialización
+		msg = "Hasta luego"; // Asignación
+
+				
+		
+		
+
+	}
+
+}
---
+++ ejemplos/3.5_Constantes/src/variables/EjemploConstantes.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class EjemploConstantes {
+
+	public static void main(String[] args) {
+		
+		final float PI = 3.141592f;
+		//PI = 3;
+		
+		final int unidad = 1;
+		//unidad = 2;
+		
+		final String msg = "Hola Mundo";
+		//msg = "Hasta luego!";
+
+	}
+
+}
