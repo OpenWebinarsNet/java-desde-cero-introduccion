d13cb74276a7fee21bbe5fc2e423cf1b68219e7f
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2023-04-27 07:52:53 +0200

    Hasta 4.06

--- ejemplos/3.01_Variables/src/variables/EjemploVariables.java
+++ ejemplos/3.01_Variables/src/variables/EjemploVariables.java
@@ -3,18 +3,13 @@ package variables;
 public class EjemploVariables {
 
 	public static void main(String[] args) {
-		
-				
+						
 		// Declaración, inicialización y asignación
 		
 		String msg = "Hola Mundo";  // Declaración e inicialización
 		int x; // Declaración
 		x = 7;  // Inicialización
 		msg = "Hasta luego"; // Asignación
-
-				
-		
-		
 
 	}
 
--- ejemplos/3.10_Ejercicio1/src/variables/operadores/Ejercicio01.java
+++ ejemplos/3.10_Ejercicio1/src/variables/operadores/Ejercicio01.java
@@ -2,6 +2,22 @@ package variables.operadores;
 
 import java.util.Scanner;
 
+/*
+El precio de final de venta de un ordenador portátil es la suma total de:
+
+- El coste de fabricación
+- El margen (en porcentaje) de beneficio de ganancia del vendedor
+- Los impuestos (calculados sobre el coste de fabricación más el margen de beneficio).
+
+Si suponemos que la ganancia del vendedor va a ser del 30% de cada ordenador, y que el impuesto aplicable es del 21%, diseñar un programa que calcule el precio final del portátil y lo muestre por pantalla. El precio del coste de fabricación se debe obtener leyéndolo desde el teclado.
+
+Además de mostrar el precio final por consola, debe mostrar si el portátil tiene un precio NORMAL o DE LUJO mediante la siguiente fórmula:
+
+- Precio final <= 600 : NORMAL
+- Precio final > 600 : DE LUJO
+  
+*/
+
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
